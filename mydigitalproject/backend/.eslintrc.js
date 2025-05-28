97e2b8a7ad01ca849818f9caa4f46559c92a3a63
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2025-05-28 16:44:49 +0200

    deleted some files

--- mydigitalproject/backend/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: ['react'],
-    extends: ['plugin:react/recommended', 'next', 'next/core-web-vitals'],
-    rules: {
-        'react/no-unescaped-entities': 'off',
-    },
-};
--- mydigitalproject/backend/prisma/migrations/20250517160248_add_active_status/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "CompanyProfile" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "token" TEXT;
-
--- AlterTable
-ALTER TABLE "WorkerProfile" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "token" TEXT;
